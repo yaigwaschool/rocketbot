0f4c300c3ccd85a3f6f98ec3ea11cf45e3dce6a1
Seb_105 <sebastianschapfel105@gmail.com>
2023-12-07 19:15:48 +0000

    I DID IT!

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const https = require("https")
+const app = express()
+
+app.get("/*", (req, res, next) => {
+  https.request(new URL("https://rocketbotroyale.winterpixel.io" + req.path), (resp) => {
+    res.contentType(resp.headers["content-type"])
+    resp.pipe(res)
+  }).end()
+})
+
+app.listen(3000)
