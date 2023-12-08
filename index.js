c78cf095508f2b5f71cc4396840dfe920c165444
Seb_105 <sebastianschapfel105@gmail.com>
2023-12-08 10:20:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.get("/discord/*", (req, res, next) => {
   const pathSegments = req.path.split('/');
-  const discordPath = pathSegments[1]
+  const discordPath = pathSegments[2]
   https.request(new URL("https://discord.com/" + discordPath), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
@@ -12,7 +12,7 @@ app.get("/discord/*", (req, res, next) => {
 })
 app.get("/rocket/*", (req, res, next) => {
   const pathSegments = req.path.split('/');
-  const rocketPath = pathSegments[1]
+  const rocketPath = pathSegments[2]
   https.request(new URL("https://rocketbotroyale.winterpixel.io" + rocketPath), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
