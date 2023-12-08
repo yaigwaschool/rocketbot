6ed321189107afe2312b2cdf7aabcb08aebc98cd
Seb_105 <sebastianschapfel105@gmail.com>
2023-12-08 10:09:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require("express")
 const https = require("https")
 const app = express()
 
-app.get("/*", (req, res, next) => {
-  https.request(new URL("https://rocketbotroyale.winterpixel.io" + req.path), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
 app.get("/discord/*", (req, res, next) => {
   https.request(new URL("https://discord.com" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
 })
+app.get("/*", (req, res, next) => {
+  https.request(new URL("https://rocketbotroyale.winterpixel.io" + req.path), (resp) => {
+    res.contentType(resp.headers["content-type"])
+    resp.pipe(res)
+  }).end()
+})
+
 
 app.listen(3000)
