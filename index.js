34e2392c8b1f56958a543cf1cbf80d86f0a972d3
Seb_105 <sebastianschapfel105@gmail.com>
2023-12-08 10:24:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.get("/discord/*", (req, res, next) => {
     resp.pipe(res)
   }).end()
 })
-app.get("/*", (req, res, next) => {
+app.get("/rocket/*", (req, res, next) => {
   const pathSegments = req.path.split('/');
   const rocketPath = pathSegments[2]
-  https.request(new URL("https://rocketbotroyale.winterpixel.io" + rocketPath), (resp) => {
+  https.request(new URL("https://rocketbotroyale.winterpixel.io/" + rocketPath), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
