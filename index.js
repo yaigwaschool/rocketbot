f352f0f3a52f5465c8e3c61570b3fea5763d7a6b
Sebastian <sebastianschapfel105@gmail.com>
2024-03-14 17:00:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-const http = require('http');
-const https = require('https');
-const { parse } = require('url');
+const express = require("express")
+const https = require("https")
+const app = express()
 
-// Create a proxy server
-const proxyServer = http.createServer((req, res) => {
-    // Parse the request URL
-    const { pathname, query } = parse(req.url, true);
+app.get("/*", (req, res, next) => {
+  //const pathSegments = req.path.split('/');
+ // const rocketPath = pathSegments[2]
+  https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
+    res.contentType(resp.headers["content-type"])
+    resp.pipe(res)
+  }).end()
+})
 
-    // Options for the outgoing request
-    const options = {
-        hostname: 'rocketbotroyale2.winterpixel.io',
-        path: pathname + (query ? '?' + query : ''),
-        method: req.method,
-        headers: req.headers
-    };
 
-    // Send the request to the external server
-    const proxyReq = https.request(options, (proxyRes) => {
-        // Set the response headers
-        res.writeHead(proxyRes.statusCode, proxyRes.headers);
-        
-        // Pipe the response from the external server to the client
-        proxyRes.pipe(res);
-    });
-
-    // Handle errors
-    proxyReq.on('error', (error) => {
-        console.error('Proxy request error:', error);
-        res.statusCode = 500;
-        res.end('Proxy request failed');
-    });
-
-    // Pipe the request body, if any
-    req.pipe(proxyReq);
-});
-
-// Start the proxy server
-const PORT = process.env.PORT || 3000;
-proxyServer.listen(PORT, () => {
-    console.log(`Proxy server is running on port ${PORT}`);
-});
+app.listen(3000)
