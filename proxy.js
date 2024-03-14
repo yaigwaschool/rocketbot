ed9bc4f33d918906a0d54503e00965ea23920337
Sebastian <sebastianschapfel105@gmail.com>
2024-03-14 17:07:28 +0000

    f

---
+++ proxy.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const https = require("https")
+const app = express()
+
+app.get("/*", (req, res, next) => {
+const pathSegments = req.path.split('/');
+const rocketPath = pathSegments[2]
+  https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
+    res.contentType(resp.headers["content-type"])
+    resp.pipe(res)
+  }).end()
+})
+app.listen(3000)
