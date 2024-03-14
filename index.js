0fbf69119ffa1b139ed4281e6aca512d35321597
Sebastian <sebastianschapfel105@gmail.com>
2024-03-14 15:41:17 +0000

    ff

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const express = require("express")
+const fetch = require("node-fetch")
 const https = require("https")
 const app = express()
 
-app.get("/chess/*", (req, res, next) => {
-  const pathSegments = req.path.split('/');
-  const discordPath = pathSegments[2]
-  https.request(new URL("https://chess.com/" + discordPath), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
 app.get("/*", (req, res, next) => {
-  //const pathSegments = req.path.split('/');
- // const rocketPath = pathSegments[2]
-  https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
+  https.request(new URL("https://turbowarp.org" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
 })
-
 
 app.listen(3000)
