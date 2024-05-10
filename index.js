9663b6ab44ca190a5f08949954277cb703f86353
yaigwaschool <163452637+yaigwaschool@users.noreply.github.com>
2024-05-10 14:36:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require("express")
 const https = require("https")
 const app = express()
 
+app.get("/chess/*", (req, res, next) => {
+  const pathSegments = req.path.split('/');
+  const discordPath = pathSegments[2]
+  https.request(new URL("https://chess.com/" + discordPath), (resp) => {
+    res.contentType(resp.headers["content-type"])
+    resp.pipe(res)
+  }).end()
+})
 app.get("/*", (req, res, next) => {
   //const pathSegments = req.path.split('/');
  // const rocketPath = pathSegments[2]
-  https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
+  https.request(new URL("https://geforcenow.com/" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
