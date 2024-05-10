016ead9fabed31effe179ec7ab35eceaa0695009
yaigwaschool <163452637+yaigwaschool@users.noreply.github.com>
2024-05-10 14:38:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/chess/*", (req, res, next) => {
 app.get("/*", (req, res, next) => {
   //const pathSegments = req.path.split('/');
  // const rocketPath = pathSegments[2]
-  https.request(new URL("https://geforcenow.com/" + req.path), (resp) => {
+  https.request(new URL("https://play.geforcenow.com/" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
